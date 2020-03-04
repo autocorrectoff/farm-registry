dade82f4ad61b1fcd440271a6b03ef857f6818d9
whiteboii <captainwhiteboii@gmail.com>
2020-03-04 23:02:34 +0100

    hibernate is not almighy; native query to the rescue

--- src/main/java/com/mb/farmregistry/repositories/FarmRepository.java
+++ src/main/java/com/mb/farmregistry/repositories/FarmRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 public interface FarmRepository extends JpaRepository<Farm, Long> {
 
-    @Query("select f from Farm f join f.users u where u.id=:userId")
+    @Query(value = "select f.id, f.name, f.customer_id from farm f join user_farm uf on f.id=uf.farm_id where uf.user_id=:userId", nativeQuery = true)
     List<Farm> findByUserId(@Param("userId") Long userId);
 }
