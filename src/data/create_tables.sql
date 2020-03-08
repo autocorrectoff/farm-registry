0bb55d25fd2ef79515109ff071ca1d229b7eb83e
whiteboii <captainwhiteboii@gmail.com>
2020-03-08 12:42:59 +0100

    more useful sample data

--- src/data/create_tables.sql
+++ src/data/create_tables.sql
@@ -19,4 +19,4 @@ insert into customer(name) values('Old McDonald'), ('Dorothy');
 insert into farm(name, customer_id) values('Old McDonald''s farm', 1), ('Uncle Owen''s moisture farm', 1), ('Dorothy''s farm', 2);
                                                                           -- 123                                                                                        -- 234
 insert into "user"(email, password, is_admin) values('test@example.com', '$2a$12$XrJE4UO6n53r3dDUaOf4Ce0N0NZIvRKnGyA7iLgecpJI.kuVXZ.z2', false), ('test1@example.com', '$2a$12$P6Fnf/48lRHunxV7t1VlWO6ywH70u9BLULUwsRVTS0PLZhtL0JjU.', false);
-insert into user_farm(user_id, farm_id) values(1, 1), (2, 2), (3, 2);
+insert into user_farm(user_id, farm_id) values(1, 1), (2, 3), (1, 2);
